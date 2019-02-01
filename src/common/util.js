4e6895d69e891e1a00fbf72c989fbe4909e45589
choi hoon chul <hosoon2@gmail.com>
2019-02-01 15:36:50 +0900

    Create util.js

---
+++ src/common/util.js
@@ -0,0 +1,23 @@
+const urlExp = (url) => {
+  let myRegExp  =/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  return myRegExp.test(url);
+};
+
+const curry = fn => {
+  const arity = fn.length
+
+  return (function resolver () {
+    let memory = Array.prototype.slice.call(arguments)
+    return function () {
+      let local = memory.slice()
+      Array.prototype.push.apply(local, arguments)
+      let next = local.length >= arity ? fn : resolver
+      return next.apply(null, local)
+    }
+  })()
+}
+
+
+export { urlExp, curry }
+
