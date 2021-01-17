f6415f2a23281037b86a9cccf12d47dacbd2fad8
hosoon2 <hosoon2@gmail.com>
2021-01-17 15:37:05 +0900

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -107,7 +107,7 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
+/* if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
@@ -126,4 +126,4 @@ if (process.env.NODE_ENV === 'production') {
       minimize: true
     })
   ])
-}
+} */
